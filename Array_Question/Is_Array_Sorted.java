b068d090f34cc31406a35c800892b2af1ed54e16
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2023-03-02 23:37:20 +0530

    Add files via upload

---
+++ Array_Question/Is_Array_Sorted.java
@@ -0,0 +1,38 @@
+package Array_Question;
+
+import java.util.Scanner;
+
+public class Is_Array_Sorted {
+
+    static boolean isArraySorted(int[] arr){
+        for(int i = 0;i<arr.length-1;i++){
+            if (arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the elements of Array: ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean b = isArraySorted(arr);
+
+        if(b){
+            System.out.println("Sorted Array");
+        }
+        else{
+            System.out.println("Unsorted Array");
+        }
+
+        sc.close();
+    }
+
+}
