1758b18da34df6c3cab4bdf55f7df0b38328cb2a
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2023-03-07 00:41:17 +0530

    Add files via upload

---
+++ Array_Question/Find_span.java
@@ -0,0 +1,40 @@
+package Array_Question;
+
+import java.util.Scanner;
+
+class Find_span{
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the elements of Array: ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int span = findSpan(arr);
+
+        System.out.println("Span of array is: " + span);
+
+        sc.close();
+    }
+
+    private static int findSpan(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i = 0;i<arr.length; i ++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+            if(min > arr[i]){
+                min = arr[i];
+            }
+
+        }
+        return (max - min);
+    }
+
+}
