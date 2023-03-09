0b36d66a23894404bed61cd3af93915cae530e9c
Nishant Sharma <96777814+nishantsharma13903@users.noreply.github.com>
2023-03-09 20:46:58 +0530

    Add files via upload

---
+++ Array_Question/LeaderElement.java
@@ -0,0 +1,39 @@
+package Array_Question;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LeaderElement {
+
+    static ArrayList<Integer> leaderArray(int[] arr){
+        ArrayList<Integer> leader = new ArrayList<>();
+        leader.add(arr[arr.length - 1]);
+        int max = arr[arr.length - 1];
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(arr[i] > max){
+                leader.add(arr[i]);
+                max = arr[i];
+            }
+        }
+
+        return leader;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the elements of array : ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        ArrayList<Integer> leader = leaderArray(arr);
+
+        System.out.println("Leader Array is : " + leader);
+
+        sc.close();
+    }
+
+}
